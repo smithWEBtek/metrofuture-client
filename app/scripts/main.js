8e02d1de4d06c9a9edd1d8db771de138ae594230
allthesignals <wilburnforce@gmail.com>
2015-08-08 15:46:06 -0400

    Changed API endpoint for images in the templates.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -9,7 +9,7 @@ window.MfiaClient = {
   init: function () {
 
     //API Endpoint
-    MfiaClient.API = "http://mfia.dev.mapc.org"
+    MfiaClient.API = "http://mfia.prep.mapc.org"
 
     //intitialize Marionette app
     this.app = new Marionette.Application();
