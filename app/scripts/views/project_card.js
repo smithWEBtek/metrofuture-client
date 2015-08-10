f646db2d8ec6e869c71e22f9847c147e8cd00f01
allthesignals <wilburnforce@gmail.com>
2015-08-10 14:32:15 -0400

    Reworked project card design.

--- app/scripts/views/project_card.js
+++ app/scripts/views/project_card.js
@@ -24,6 +24,15 @@ MfiaClient.Views = MfiaClient.Views || {};
         render: function () {
             this.$el.html(this.template(this.model.toJSON()));
             return this;
+        },
+        slugify: function(text) {
+            return text.toString().toLowerCase()
+                .replace(/\s+/g, '-')           // Replace spaces with -
+                .replace(/[^\w\-]+/g, '')       // Remove all non-word chars
+                .replace(/\-\-+/g, '-')         // Replace multiple - with single -
+                .replace(/^-+/, '')             // Trim - from start of text
+                .replace(/-+$/, '');            // Trim - from end of text
+
         }
 
     });
