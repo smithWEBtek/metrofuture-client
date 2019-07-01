db5f5c308695d963bcc6317795bec27ddf37976b
Brad Smith <brad@smithwebtek.com>
2019-07-01 14:50:11 -0400

    Project card colors by project_geography

--- app/scripts/collections/municipalities.js
+++ app/scripts/collections/municipalities.js
@@ -5,13 +5,14 @@ MfiaClient.Collections = MfiaClient.Collections || {};
 (function () {
   MfiaClient.Collections.Municipalities = Backbone.Collection.extend({
 
-      model: MfiaClient.Models.Municipality,
-      url: function () {
-        return  MfiaClient.API + '/municipalities?filter[mapc]=true&page[size]=351';
-      },
-      parse: function(response) {
-        return response.data;
-      }
+    model: MfiaClient.Models.Municipality,
+    url: function () {
+      // return  MfiaClient.API + '/municipalities?filter[mapc]=true&page[size]=351';
+      return MfiaClient.API + '/municipalities';
+    },
+    parse: function (response) {
+      return response.data;
+    }
 
   });
 })();
